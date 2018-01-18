8544bf63fcddc37be0dd34de004375183ee14229
Stefan van der Walt <stefanv@berkeley.edu>
2018-01-18 04:07:29 -0800

    Simpler numpy array syntax; forget stride for now

--- demos/numpy_basic/numpy_basic.pyx
+++ demos/numpy_basic/numpy_basic.pyx
@@ -1,10 +1,10 @@
 cimport numpy as cnp
 import numpy as np
 
-def foo(double[:, ::1] arr):
+def foo(double[:, :] arr):
 
     cdef Py_ssize_t i, j
-    #cdef double[:, ::1] out = np.zeros_like(arr)
+    #cdef double[:, :] out = np.zeros_like(arr)
 
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
